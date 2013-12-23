077de489b2b3a730f48bc98e83337f9e43c4ed7b
Mingjun Zhou <mingjun.zhou@gmail.com>
2013-12-23 15:38:37 +0800

    yields words

--- ch2/load_own_corpus.py
+++
@@ -1,6 +0,0 @@
-from nltk.corpus import PlaintextCorpusReader
-
-corpus_root = '/usr/share/dict'
-wordlists = PlaintextCorpusReader(corpus_root, '.*')
-print wordlists.fileids()
-print wordlists.words('words')
