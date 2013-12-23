e36061eb422066a6d02baac5282e19b638535ecd
Mingjun Zhou <mingjun.zhou@gmail.com>
2013-12-23 14:50:15 +0800

    initialized

---
+++ ch2/americans_vs_citizens.py
@@ -0,0 +1,11 @@
+import nltk
+from nltk.corpus import inaugural
+
+
+cfd = nltk.ConditionalFreqDist(
+        (target, fileid[:4])
+        for fileid in inaugural.fileids()
+        for w in inaugural.words(fileid)
+        for target in ['america', 'citizen']
+        if w.lower().startswith(target))
+cfd.plot()
---
+++ ch2/load_own_corpus.py
@@ -0,0 +1,6 @@
+from nltk.corpus import PlaintextCorpusReader
+
+corpus_root = '/usr/share/dict'
+wordlists = PlaintextCorpusReader(corpus_root, '.*')
+print wordlists.fileids()
+print wordlists.words('words')
